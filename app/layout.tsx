e4a6d924013d83c708d9d18da39d54f87afc1a4b
alfitosantosa <sanskun02pg@gmail.com>
2024-12-21 12:32:43 +0700

    push analithics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Analytics } from "@vercel/analytics/next";
 // import { DockDemo } from "@/components/dock-provider";
 
 const geistSans = localFont({
@@ -33,7 +34,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-          {children}
+          {children} <Analytics />
         </ThemeProvider>
       </body>
     </html>
