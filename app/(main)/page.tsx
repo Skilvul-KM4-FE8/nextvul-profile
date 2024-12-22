09fd75ea0a8652953a76276077dc78575848a813
alfitosantosa <sanskun02pg@gmail.com>
2024-12-22 23:41:49 +0700

    push ui landing

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,76 +1,70 @@
-import Image from "next/image";
-
+"use client";
 import { MarqueeDemo } from "../../components/magic-card-provider";
 import { DockDemo } from "@/components/dock-provider";
 import Footer from "@/components/footer";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { useState } from "react";
+
+import { CircleLoader, PuffLoader, RingLoader } from "react-spinners";
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+
   return (
     <>
       <DockDemo />
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-          <Image className="dark:invert" src="/next.svg" alt="Next.js logo" width={180} height={38} priority />
-          <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-            <li className="mb-2">
-              Get started by editing <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">app/page.tsx</code>.
-            </li>
-            <li>Save and see your changes instantly.</li>
-          </ol>
+      <div>
+        {/* main 0 */}
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center mx-auto max-w-5xl max-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-mono)]">
+          <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+            <p className="text-center sm:text-left text-3xl font-bold"> Nextvul, Indonesia's leading software development agency </p>
 
-          <div className="flex gap-4 items-center flex-col sm:flex-row">
-            <a
-              className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image className="dark:invert" src="/vercel.svg" alt="Vercel logomark" width={20} height={20} />
-              Deploy now
-            </a>
-            <a
-              className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Collabs with us
-            </a>
-          </div>
-        </main>
-        <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-          <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image aria-hidden src="/file.svg" alt="File icon" width={16} height={16} />
-            Learn
-          </a>
-          <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image aria-hidden src="/window.svg" alt="Window icon" width={16} height={16} />
-            Examples
-          </a>
-          <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image aria-hidden src="/globe.svg" alt="Globe icon" width={16} height={16} />
-            Go to nextjs.org →
-          </a>
-        </footer>
+            <p className="text-center sm:text-left">
+              Founded in Jakarta, Nextvul is the We design, build and maintain software and their backends for most of the leading companies in various industries, like financial services, logistics and transportation, e-commerce,
+              entertainment, loyalty and many more.
+            </p>
+            <div className="flex justify-center text-center">
+              <PuffLoader color={"#047CB6"} loading={loading} />
+            </div>
+          </main>
+        </div>
+        {/* main 1 */}
+        <div className="grid grid-rows-[20px_1fr_20px] items-center mx-auto max-w-5xl justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-mono)]">
+          <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+            <p className="text-center sm:text-left text-xl font-bold"> Alasan Memilih Nextvul </p>
+
+            <p className="text-center sm:text-left">Konsultan IT Terbaik di Indonesia, Berikan Solusi Terdepan untuk Bisnis Anda</p>
+            <p className="text-center sm:text-left">Nextvul tidak hanya menawarkan solusi teknologi terdepan, namun juga kemitraan yang didasarkan pemahaman mendalam mengenai kebutuhan unik industri bisnis Anda.</p>
+            <Card>
+              <CardHeader>
+                <CardTitle>Tim Ahli Berpengalaman</CardTitle>
+                <CardDescription>Tim ahli IT yang berpengalaman yang siap membantu menemukan solusi IT yang tepat.</CardDescription>
+              </CardHeader>
+            </Card>
+            <Card>
+              <CardHeader>
+                <CardTitle>Teknologi Terdepan</CardTitle>
+                <CardDescription>Memberikan akses ke teknologi terbaru dan inovatif untuk membantu Anda jadi terdepan.</CardDescription>
+              </CardHeader>
+            </Card>
+            <Card>
+              <CardHeader>
+                <CardTitle>Komitmen Terhadap Kualitas</CardTitle>
+                <CardDescription>Pengalaman bertahun-tahun memberikan layanan terbaik dengan standar kualitas tinggi.</CardDescription>
+              </CardHeader>
+            </Card>
+            <Card>
+              <CardHeader>
+                <CardTitle>Fokus pada Pelanggan</CardTitle>
+                <CardDescription>Memprioritaskan kebutuhan Anda untuk berikan solusi yang sesuai dengan bisnis Anda.</CardDescription>
+              </CardHeader>
+            </Card>
+          </main>
+        </div>
+
+        <MarqueeDemo />
+        <Footer />
       </div>
-
-      <MarqueeDemo />
-      <Footer />
     </>
   );
 }
