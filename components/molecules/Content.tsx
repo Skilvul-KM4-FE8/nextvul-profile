72e48e18cc9589edfea54b65d025d58209798b55
alfitosantosa <sanskun02pg@gmail.com>
2025-01-07 00:00:10 +0700

    fix lint alfito

--- components/molecules/Content.tsx
+++ components/molecules/Content.tsx
@@ -1,4 +1,4 @@
-import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardDescription, CardTitle } from "@/components/ui/card";
 export default function Content() {
   const data = [
     {
