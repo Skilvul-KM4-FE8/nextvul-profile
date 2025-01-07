3edbc8d742c0938ed1e8ed06c83a05e7fcb92fe8
alfitosantosa <sanskun02pg@gmail.com>
2025-01-07 23:13:31 +0700

    push icons content

--- components/molecules/Content.tsx
+++ components/molecules/Content.tsx
@@ -1,21 +1,26 @@
 import { Card, CardDescription, CardTitle } from "@/components/ui/card";
+import { icons, UserRoundCheck } from "lucide-react";
 export default function Content() {
   const data = [
     {
-      title: "Tim Ahli Berpengalaman",
-      description: "Tim ahli IT yang berpengalaman yang siap membantu menemukan solusi IT yang tepat.",
+      title: "Keahlian dan Dedikasi",
+      description: "Kami memiliki tim ahli dengan pengalaman bertahun-tahun yang berkomitmen untuk memberikan hasil terbaik bagi klien",
+      icons: icons.Zap,
     },
     {
-      title: "Teknologi Terdepan",
-      description: "Memberikan akses ke teknologi terbaru dan inovatif untuk membantu Anda jadi terdepan.",
+      title: "Konsistensi",
+      description: "Nextvul bekerja bahu-membahu dengan Anda untuk memastikan solusi kami tidak hanya relevan tetapi juga praktis untuk diterapkan",
+      icons: icons.UserRoundCheck,
     },
     {
-      title: "Komitmen Terhadap Kualitas",
-      description: "Pengalaman bertahun-tahun memberikan layanan terbaik dengan standar kualitas tinggi.",
+      title: "Inovasi Berkelanjutan",
+      description: "Kami selalu berada di garis depan inovasi teknologi, sehingga bisnis Anda tetap selangkah lebih maju",
+      icons: icons.ChartNoAxesCombined,
     },
     {
-      title: "Fokus pada Pelanggan",
-      description: "Memprioritaskan kebutuhan Anda untuk berikan solusi yang sesuai dengan bisnis Anda.",
+      title: "Terbaik di Ranahnya",
+      description: "Dengan harga yang terjangkau, pelayanan yang kami berikan tidak kalah bersaing dengan software lainnya",
+      icons: icons.HandCoins,
     },
   ];
   return (
@@ -26,12 +31,13 @@ export default function Content() {
           <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
             <p className="text-center sm:text-left text-4xl font-bold">Alasan Memilih Nextvul</p>
             <p className="text-center text-2xl  sm:text-left">Konsultan IT Terbaik di Indonesia, Berikan Solusi Terdepan untuk Bisnis Anda</p>
-            <p className="text-center sm:text-left">Nextvul tidak hanya menawarkan solusi teknologi terdepan, namun juga kemitraan yang didasarkan pemahaman mendalam mengenai kebutuhan unik industri bisnis Anda.</p>{" "}
+            <p className="text-center sm:text-left">Nextvul tidak hanya menawarkan solusi teknologi terdepan, namun juga kemitraan yang didasarkan pemahaman mendalam mengenai kebutuhan unik industri bisnis Anda.</p>
           </main>{" "}
         </div>
         <div className=" grid grid-cols-2 gap-2">
           {data.map((item, index) => (
             <Card className="p-4" key={index}>
+              <item.icons size={30} className="my-3" />
               <CardTitle className="mb-3 underline">{item.title}</CardTitle>
               <CardDescription>{item.description}</CardDescription>
             </Card>
