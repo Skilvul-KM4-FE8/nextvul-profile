52a5b40410e92acc98951e200225e7e6491f0561
Muhamad Alfito <muhamad.alfito@id.apac.local>
2025-03-04 15:17:21 +0700

    push marketplace ui

--- app/marketplace/page.tsx
+++ app/marketplace/page.tsx
@@ -6,17 +6,27 @@ import { useGetProducts } from "@/features/marketplace/products/api/use-get-prod
 import Image from "next/image";
 import TypescriptIcon from "@/public/typescript.png";
 
+import { useCartStore } from "@/store/cart-store"; // Import store
+import DropdownCart from "@/components/molecules/dropdownCart";
+
 export default function Martketplace() {
   const productsQuery = useGetProducts();
   const productsData = productsQuery.data || [];
-  console.log(productsData);
+  const { addToCart, cart } = useCartStore(); // store
 
   return (
     <>
       <DockDemo />
       <div className="flex flex-col items-center justify-center min-h-screen mt-20 py-10 font-[family-name:var(--font-geist-sans)] p-4">
-        <h1 className="text-2xl font-bold ">Marketplace</h1>
-        <p className="text-gray-500 mb-10">Welcome to Marketplace page</p>
+        <div className="flex w-full px-5 max-w-6xl  mt-10">
+          <div className="justify-items-start">
+            <h1 className="text-2xl  font-bold ">Marketplace</h1>
+            <p className="text-gray-500 mb-10">Welcome to Marketplace page</p>
+          </div>
+          <div className="justify-items-end ml-auto">
+            <DropdownCart />
+          </div>
+        </div>
 
         {productsQuery.isLoading && <p>Loading...</p>}
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 w-full max-w-6xl mt-4">
@@ -26,7 +36,9 @@ export default function Martketplace() {
               <h2 className="text-lg font-semibold">{product.name}</h2>
               <p>{product.description}</p>
               <p>Rp {new Intl.NumberFormat("id-ID").format(product.price)}</p>
-              <Button className="mt-2 w-full">Buy now</Button>
+              <Button className="mt-2 w-full" onClick={() => addToCart({ ...product, id: index, quantity: 1 })}>
+                Add to Cart
+              </Button>
             </Card>
           ))}
         </div>
---
+++ components/molecules/dropdownCart.tsx
@@ -0,0 +1,42 @@
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuShortcut, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { useCartStore } from "@/store/cart-store";
+import { Button } from "../ui/button";
+import { ShoppingCart } from "lucide-react";
+
+export default function DropdownCart() {
+  const { cart, clearCart } = useCartStore();
+
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <div className="">
+            <ShoppingCart size={20} />
+            <p className="text-sm text-gray-500"> {cart.length ? <div> ({cart.length})</div> : <div></div>}</p>
+          </div>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <div className="p-4">
+            {cart.length > 0 ? (
+              cart.map((item) => (
+                <div key={item.id} className=" ">
+                  <DropdownMenuItem>
+                    {item.name} <DropdownMenuShortcut>{item.quantity}</DropdownMenuShortcut>
+                  </DropdownMenuItem>
+                </div>
+              ))
+            ) : (
+              <p>Cart is empty</p>
+            )}
+            <DropdownMenuSeparator />
+            <Button className="">Checkout</Button>{" "}
+            <Button variant="destructive" onClick={clearCart}>
+              Clear Cart
+            </Button>
+            <DropdownMenuSeparator />
+          </div>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+}
