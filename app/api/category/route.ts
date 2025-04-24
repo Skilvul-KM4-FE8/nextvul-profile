59744f8f372d28682a36b0b5481660768373fefb
Muhamad Alfito Santosa <alfitosantosa@Muhamads-MacBook-Air.local>
2025-04-24 09:38:17 +0700

    push lint 4

--- app/api/category/route.ts
+++ app/api/category/route.ts
@@ -1,34 +1,33 @@
-// import { NextRequest, NextResponse } from "next/server";
-// import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
 
-// const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
-//post user
-// export async function POST(req: NextRequest) {
-//   const { clerkUserId, email, name } = await req.json();
-//   try {
-//     const user = await prisma.user.create({
-//       data: {
-//         clerkUserId: clerkUserId,
-//         email: email,
-//         name: name,
-//       },
-//     });
+// create categories
+export async function POST(req: NextRequest) {
+  const { name, description } = await req.json();
+  try {
+    const category = await prisma.category.create({
+      data: {
+        name: name,
+        description: description,
+      },
+    });
 
-//     return NextResponse.json({ status: "success", clerkUserId, email, name, user });
-//   } catch (error) {
-//     console.error("Error creating user:", error);
-//     return new Response("Something error", { status: 501 });
-//   }
-// }
+    return NextResponse.json({ status: "success", category });
+  } catch (error) {
+    console.error("Error creating user:", error);
+    return new Response("Something error", { status: 501 });
+  }
+}
 
-//get user
-// export async function GET(req: NextRequest) {
-//   try {
-//     const users = await prisma.category.findMany();
-//     return NextResponse.json(users);
-//   } catch (error) {
-//     console.error("Error fetching users:", error);
-//     return NextResponse.json({ error: "Internal server error" }, { status: 500 });
-//   }
-// }
+//get category
+export async function GET() {
+  try {
+    const categories = await prisma.category.findMany();
+    return NextResponse.json(categories);
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    return NextResponse.json({ error: "Internal server error" }, { status: 500 });
+  }
+}
